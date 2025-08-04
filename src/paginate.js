1e9fab72a76c557a1590334aa15abe16e83618e1
Muhammad Afzaal Chohan <afzaalchohan74@gmail.com>
2025-08-04 10:39:51 +0500

    deleted unnecessary files

--- src/paginate.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.paginate = paginate;
-function paginate(items, pageSize, pageNumber) {
-    var startIndex = pageNumber * pageSize - pageSize;
-    var endIndex = pageNumber * pageSize;
-    return items.slice(startIndex, endIndex);
-}
